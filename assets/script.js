6670cf4f77e7b785a5f61290642ec7196da53017
Ienxternal <alexanderttnguyen@gmail.com>
2023-04-10 07:48:15 -0500

    refactored index/js file

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ const hour9 = document.getElementById("hour-9");
 let date = new Date();
 let hour = date.getHours();
 
+
+//Criteria for color selection of past, present and future
 $('.time-block').each(function() {
   let block = parseInt($(this).attr("id"));
   if (block == hour) {
@@ -24,12 +26,14 @@ $('.time-block').each(function() {
   
 
 $(document).ready(function() {
+  //Add click functionality to saveBtn and save
   $('.saveBtn').on("click", function(e) {
     e.preventDefault();
     let id = $(this).parent().attr("id");
     let text = $(this).siblings(".description").val();
     localStorage.setItem(id, text);
   });
+  //On Load load local storage relevant to time slot.
   $("#9 .description").val(localStorage.getItem("9"));
   $("#10 .description").val(localStorage.getItem("10"));
   $("#11 .description").val(localStorage.getItem("11"));
@@ -43,6 +47,7 @@ $(document).ready(function() {
   showDateTime();
 });
 
+//Display date on screen
 function showDateTime() {
   var dayList = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var monthNames = [
